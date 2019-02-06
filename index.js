3e516bac00e95b22e553a57c3ddc622531f8b08b
Arty <26905074+artysidorenko@users.noreply.github.com>
2019-02-06 00:47:41 +0000

    remove css sanitize

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import App from './components/app';
-import "sanitize.css/sanitize.css";
+// import "sanitize.css/sanitize.css";
 
 render(<App />, document.getElementById("root"));
